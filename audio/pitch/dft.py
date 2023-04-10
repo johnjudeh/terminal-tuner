339016898bf90f75bcb5fc156ebfcb97a4cc750d
John Judeh <johnjudeh@hotmail.com>
2023-04-10 13:26:38 +0100

    feat: Add safety in dft method for high-amplitude noise like the mains hum

--- audio/pitch/dft.py
+++ audio/pitch/dft.py
@@ -2,14 +2,52 @@ from audio.utils import find_nearest_note
 from scipy.fft import fft, fftfreq
 from scipy.io import wavfile
 import numpy as np
+from math import ceil
+from typing import Optional
+
+
+def _find_base_frequency(frequencies: list[float]) -> Optional[float]:
+    """
+    Given a list of frequencies, finds the base frequency and returns it. Because
+    strings vibrate at frequencies that can form standing waves, the base frequency
+    will be the lowest frequency where other major frequencies are multiples of it
+    (overtones). This function finds the lowest frequency where the majority of the
+    other frequencies are multiples of that (within a certain threshold)
+    """
+    MULTIPLE_THRESHOLD = 0.05
+    majority_of_frequencies = ceil(len(frequencies) / 2)
+    cleaned_frequencies = sorted(frequencies)
+
+    while True:
+        if len(cleaned_frequencies) == 0:
+            return None
+
+        base_freq_candidate = cleaned_frequencies[0]
+
+        factored_frequencies = [f / base_freq_candidate for f in cleaned_frequencies]
+        factored_frequencies_errors = [abs(f - round(f)) for f in factored_frequencies]
+        count_of_passing_frequencies = sum(
+            1 if f <= MULTIPLE_THRESHOLD else 0 for f in factored_frequencies_errors
+        )
+        if count_of_passing_frequencies >= majority_of_frequencies:
+            return base_freq_candidate
+        else:
+            cleaned_frequencies.remove(base_freq_candidate)
 
 
 def calculate_pitch(filename: str) -> str:
+    """
+    Given a wave file with a musical string being played, finds the pitch of the of
+    the note it's tuned to. Uses the Discrete Fourier Transform (DFT) method to
+    identify which sine and cosine waves make up the discrete repeating function
+    passed to it. From there, it shows the frequencies of those componenets, from
+    which we can calculate the base frequency which is the string being played.
+    """
     frame_rate, audio_data = wavfile.read(filename)
     sample_size = len(audio_data)
 
-    # Use a Discrete Fourier Transform to find the frequencies that
-    # make up this audio data. Throwing away negative half.
+    # Use a Discrete Fourier Transform to find the frequencies components of
+    # this audio data, throwing away negative half.
     x_freq = fftfreq(sample_size, 1 / frame_rate)[: sample_size // 2]
     y_freq = fft(audio_data)[: sample_size // 2]
     y_freq = np.abs(y_freq)
@@ -45,19 +83,6 @@ def calculate_pitch(filename: str) -> str:
         else:
             cleaned_top_frequencies.append(freq)
 
-    # Choose the lowest frequency. Since strings vibrate at frequencies
-    # that can form standing waves, the base frequency will be the lowest
-    # and all higher frequencies will be overtones.
-    # TODO: this assumes there are no stray frequencies in the mix such as the
-    # mains hum or other electrical noise. Change this to account for this
-    cleaned_top_frequencies.sort()
-    lowest_frequency = cleaned_top_frequencies[0]
-    return find_nearest_note(lowest_frequency)
-
-
-print(calculate_pitch("examples/E.wav"))
-print(calculate_pitch("examples/A.wav"))
-print(calculate_pitch("examples/D.wav"))
-print(calculate_pitch("examples/G.wav"))
-print(calculate_pitch("examples/B.wav"))
-print(calculate_pitch("examples/e_high.wav"))
+    base_frequency = _find_base_frequency(cleaned_top_frequencies)
+    base_note = find_nearest_note(base_frequency) if base_frequency else None
+    return base_note
--- tests/audio/pitch/dft.py
+++ tests/audio/pitch/dft.py
@@ -1,4 +1,4 @@
-from audio.pitch.dft import calculate_pitch
+from audio.pitch.dft import calculate_pitch, _find_base_frequency
 
 
 def test_calculate_pitch() -> None:
@@ -9,4 +9,20 @@ def test_calculate_pitch() -> None:
     calculate_pitch("examples/G.wav") == "G3"
     calculate_pitch("examples/B.wav") == "B3"
     calculate_pitch("examples/e_high.wav") == "E4"
+    print("OK")
+
+
+def test_find_base_frequency() -> None:
+    print("Testing audio.pitch._find_base_frequency...", end="")
+    # Happy path
+    assert _find_base_frequency([20.0]) == 20.0
+    assert _find_base_frequency([20.0, 40.0, 60.0]) == 20.0
+    # Sorting
+    assert _find_base_frequency([40.0, 20.0, 60.0]) == 20.0
+    # Edge case
+    assert _find_base_frequency([20.0, 41.0, 61.0]) == 20.0
+    assert _find_base_frequency([20.0, 39.0, 59.0]) == 20.0
+    # Bad frequencies
+    assert _find_base_frequency([18.0, 19.0, 20.0, 40.0, 60.0]) == 20.0
+    assert _find_base_frequency([20.0, 44.0, 66.0]) == None
     print("OK")
